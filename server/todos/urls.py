5ec716050dc322338423fc9e7cd39d94485541d3
whyshouldibother <113842019+whyshouldibother@users.noreply.github.com>
2026-02-01 17:22:45 +0545

    Combined the api endpoints into 2 model views

--- server/todos/urls.py
+++ server/todos/urls.py
@@ -5,10 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("todo/", views.todos, name="todo"),
     path("api/", views.api, name="api"),
-    path("api/todos/", views.TodoList.as_view(), name="list"),
-    path("api/todos/<int:pk>/", views.TodoShow.as_view(), name="show"),
-    path("api/todos/<int:pk>/", views.TodoUpdate.as_view(), name="update"),
-    path("api/todos/<int:pk>/", views.TodoCreate.as_view(), name="create"),
-    path("api/todos/<int:pk>/", views.TodoDelete.as_view(), name="delete")
+    path("api/todos/", views.TodoListCreate.as_view(), name="list"),
+    path("api/todos/<int:pk>/", views.TodoOperations.as_view(), name="operations"),
     
 ]
--- server/todos/views.py
+++ server/todos/views.py
@@ -4,37 +4,16 @@ from .models import Todo
 from .serializers import TodoSerializer
 
 #get /api/todos
-class TodoList(generics.ListAPIView):
+class TodoListCreate(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-    http_method_names=["get"]
 
 #get /api/todos/<id>/ 
 #id passed as pk
-class TodoShow(generics.RetrieveAPIView):
+class TodoOperations(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-    lookup_field = "id"
-    http_method_names=["get"]
 
-#put/patch /api/todos/<id>/
-class TodoUpdate(generics.UpdateAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-    # http_method_names=["put","patch"]
-    
-#post /api/todos/<id>/
-class TodoCreate(generics.CreateAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-    http_method_names=["post"]
-    
-#delete /api/todos/<id>
-class TodoDelete(generics.DestroyAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = Todo.objects.all()
-    lookup_field="id"
-    http_method_names=["delete"]
 
 # Create your views here.
 def home(request):
@@ -45,4 +24,4 @@ def todos(request):
     return render(request, "todo.html", {"todos":items})
 
 def api(request):
-    return HttpResponse( "Hello world")
+    return render(request, "api.html")
